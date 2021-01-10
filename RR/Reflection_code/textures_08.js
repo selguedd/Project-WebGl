1934c6978e2fed3664be9f7bc36425509097e8b5
nsassine <naim.sassine@hotmail.com>
2021-01-10 04:06:22 +0100

    wow

---
+++ RR/Reflection_code/textures_08.js
@@ -0,0 +1,89 @@
+var make_texture = function(gl, url) {
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+     
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+                  new Uint8Array([0, 0, 255, 255]));
+     
+    // Asynchronously load an image
+    var image = new Image();
+    image.crossOrigin = "anonymous";
+    image.src = url;
+    image.addEventListener('load', function() {
+      // Now that the image has loaded make copy it to the texture.
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+      // TODO add parameters for filtering and warping!
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    });
+
+    return texture;
+}
+
+var make_texture_cubemap = function(gl, folder_url, width=512, height=512) {
+    var texture = gl.createTexture();
+    
+    // We need to specify the type of texture we are using
+    // This is useful for the SAMPLER in the shader
+    // It will allow us to sample a point in any direction!
+    // and not only in (s,t) coordinates
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+     
+    const faceInfos = [
+      {
+        target: gl.TEXTURE_CUBE_MAP_POSITIVE_X, 
+        url: folder_url + '/posx.jpg',
+      },
+      {
+        target: gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 
+        url: folder_url + '/negx.jpg',
+      },
+      {
+        target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y, 
+        url: folder_url + '/posy.jpg',
+      },
+      {
+        target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 
+        url: folder_url + '/negy.jpg',
+      },
+      {
+        target: gl.TEXTURE_CUBE_MAP_POSITIVE_Z, 
+        url: folder_url + '/posz.jpg',
+      },
+      {
+        target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, 
+        url: folder_url + '/negz.jpg',
+      },
+    ];
+    
+    faceInfos.forEach((faceInfo) => {
+      const {target, url} = faceInfo;
+     
+      // Upload the canvas to the cubemap face.
+      // setup each face so it's immediately renderable
+const level = 0;
+    const internalFormat = gl.RGBA;
+    const format = gl.RGBA;
+    const type = gl.UNSIGNED_BYTE;
+      gl.texImage2D(target, level, internalFormat, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+     
+      // Asynchronously load an image
+      const image = new Image();
+      image.src = url;
+      image.addEventListener('load', function() {
+        // Now that the image has loaded upload it to the texture.
+        gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+        gl.texImage2D(target, level, internalFormat, format, type, image);
+        gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+      });
+    });
+    // Mipmapping for anti aliasing when we are far away from the texture
+    gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    
+    return texture;
+}
+
--- three-jsload-gltf/README.markdown
+++
@@ -1,5 +0,0 @@
-# Three.js - Load .GLTF
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/naimsassine/pen/rNMdVrm](https://codepen.io/naimsassine/pen/rNMdVrm).
-
-from: https://threejsfundamentals.org/threejs/threejs-load-gltf.html
